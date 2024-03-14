0997fa33cdfaa18e1d13e734731b1b7a73064e06
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-14 14:59:50 +0300

    feat: init spring project

---
+++ src/main/java/com/strawberries/bankbank/BankbankApplication.java
@@ -0,0 +1,13 @@
+package com.strawberries.bankbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strawberries/bankbank/BankbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strawberries.bankbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
