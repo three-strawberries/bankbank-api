0454a0ee348d497c189a76c9b274b200bb0ba481
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-18 16:30:31 +0300

    feat: create crud operations interface

---
+++ src/main/java/com/strawberries/bankbank/repository/CrudOperations.java
@@ -0,0 +1,10 @@
+package com.strawberries.bankbank.repository;
+
+import java.util.List;
+
+public interface CrudOperations<T> {
+    List<T> findAll();
+    T save(T toSave);
+    boolean update(T entity);
+
+}
