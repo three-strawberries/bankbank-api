6664b8a8e2aea4337787e5de2cbd5a7296253a2a
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-03-21 01:32:24 +0300

    chore: format to comply with Google Java Style

--- src/main/java/com/strawberries/bankbank/entity/Balance.java
+++ src/main/java/com/strawberries/bankbank/entity/Balance.java
@@ -1,17 +1,18 @@
 package com.strawberries.bankbank.entity;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
 public class Balance {
-    private int idBalance;
-    private LocalDateTime date;
-    private double amount;
-    private String type;
+  private int idBalance;
+  private LocalDateTime date;
+  private double amount;
+  private String type;
 }
--- src/main/java/com/strawberries/bankbank/entity/Transaction.java
+++ src/main/java/com/strawberries/bankbank/entity/Transaction.java
@@ -1,26 +1,24 @@
 package com.strawberries.bankbank.entity;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
 public class Transaction {
-    private int idTransaction;
-    private LocalDateTime Date;
-    private String reference;
-    private  String description;
-    private double debit;
-    private double credit;
-    private int idAccountSender;
-    private int idAccountReceiver;
-    private int idTransactionGroup;
-    private String typeTransaction;
-
-
+  private int idTransaction;
+  private LocalDateTime Date;
+  private String reference;
+  private String description;
+  private double debit;
+  private double credit;
+  private int idAccountSender;
+  private int idAccountReceiver;
+  private int idTransactionGroup;
+  private String typeTransaction;
 }
