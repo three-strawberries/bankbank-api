30c0b92944eb4c8638b244440a181fbf1e9c70b0
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-23 19:25:27 +0300

    feat: implements Transaction services

---
+++ src/main/java/com/strawberries/bankbank/service/TransactionServices.java
@@ -0,0 +1,24 @@
+package com.strawberries.bankbank.service;
+
+import com.strawberries.bankbank.entity.Transaction;
+import com.strawberries.bankbank.repository.TransactionCrudOperations;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TransactionServices {
+    private final TransactionCrudOperations transactionCrudOperations;
+
+    public TransactionServices(TransactionCrudOperations transactionCrudOperations) throws SQLException {
+        this.transactionCrudOperations = transactionCrudOperations;
+    }
+    public List<Transaction> getAllTransactions() throws SQLException {
+        return transactionCrudOperations.findAll();
+    }
+    public Transaction saveTransaction(Transaction transaction) throws SQLException {
+        return transactionCrudOperations.save(transaction);
+    }
+    public boolean updateTransaction(int idTransaction, Transaction transaction) throws SQLException {
+        return transactionCrudOperations.update(transaction);
+    }
+}
