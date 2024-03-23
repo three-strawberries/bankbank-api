13e9c19faea9df55161979f7d878ff8e923f27c4
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-23 18:23:42 +0300

    feat: create Account crud operations

---
+++ src/main/java/com/strawberries/bankbank/repository/AccountCrudOrepations.java
@@ -0,0 +1,17 @@
+package com.strawberries.bankbank.repository;
+
+
+import com.strawberries.bankbank.db.ConnectDB;
+import com.strawberries.bankbank.entity.Account;
+
+import java.sql.*;
+import java.util.List;
+
+
+public class AccountCrudOrepations extends AutoCrudOperations<Account> {
+    private final ConnectDB db = ConnectDB.getInstance();
+    private Connection connection = db.getConnection();
+    public AccountCrudOrepations(Connection connection) {
+        super(connection);
+    }
+}
