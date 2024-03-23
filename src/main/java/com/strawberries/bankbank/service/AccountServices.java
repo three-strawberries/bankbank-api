277f523543689db9888031d279f1d21979c4de9f
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-23 19:07:06 +0300

    feat: implements Account service

---
+++ src/main/java/com/strawberries/bankbank/service/AccountServices.java
@@ -0,0 +1,24 @@
+package com.strawberries.bankbank.service;
+
+import com.strawberries.bankbank.entity.Account;
+import com.strawberries.bankbank.repository.AccountCrudOrepations;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class AccountServices {
+    private final AccountCrudOrepations accountCrudOrepations;
+
+    public AccountServices(AccountCrudOrepations accountCrudOrepations) throws SQLException {
+        this.accountCrudOrepations = accountCrudOrepations;
+    }
+    public List<Account> getAllAccounts() throws SQLException {
+        return accountCrudOrepations.findAll();
+    }
+    public Account saveUser(Account account) throws SQLException {
+        return accountCrudOrepations.save(account);
+    }
+    public boolean updateAccount(int idAccount, Account account) throws SQLException {
+        return accountCrudOrepations.update(account);
+    }
+}
