194f975d4ba99dc68d02659aaafe1707caff0dfa
Salohyy212 <zerambaysalohy@gmail.com>
2024-03-24 18:15:25 +0300

    fix: use datasource instead of postgres conf

--- src/main/java/com/strawberries/bankbank/db/PostgresqlConf.java
+++
@@ -1,7 +0,0 @@
-package com.strawberries.bankbank.db;
-
-public class PostgresqlConf {
-  public static final String URL = System.getenv("BANKBANK_DB_URL");
-  public static final String USERNAME = System.getenv("BANKBANK_DB_USERNAME");
-  public static final String PASSWORD = System.getenv("BANKBANK_DB_PASSWORD");
-}
--- src/main/java/com/strawberries/bankbank/repository/AccountCrudOrepations.java
+++ src/main/java/com/strawberries/bankbank/repository/AccountCrudOrepations.java
@@ -1,18 +1,21 @@
 package com.strawberries.bankbank.repository;
 
-
-import com.strawberries.bankbank.db.ConnectDB;
 import com.strawberries.bankbank.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.sql.*;
-
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Repository
 public class AccountCrudOrepations extends AutoCrudOperations<Account> {
-    private final ConnectDB db = ConnectDB.getInstance();
-    private final Connection connection = db.getConnection();
-    public AccountCrudOrepations() {
-        super(ConnectDB.getConnection());
+        @Autowired
+        public AccountCrudOrepations(DataSource dataSource) {
+            super(dataSource);
+        }
+
     }
-}
+
+
+
